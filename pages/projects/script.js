944b97799ce4617f01d542d4b784d17854789bea
Karl Shrouder <karl.shrouder@gmail.com>
2023-01-27 10:00:56 -0500

    bug fixes

--- pages/projects/script.js
+++ pages/projects/script.js
@@ -1,11 +1,11 @@
 document.getElementById("calculator").addEventListener("click", function(){
-    window.open("./calculator/calculator.html")
+    window.open("./calculator/calculator.html", '_self')
 });
 
 document.getElementById("math").addEventListener("click", function(){
-    window.open("./math-for-kids/index.html")
+    window.open("./math-for-kids/index.html", '_self')
 });
 
 document.getElementById("unsplash").addEventListener("click", function(){
-    window.open("./unsplash/unsplash.html")
+    window.open("./unsplash/unsplash.html", '_self')
 });
