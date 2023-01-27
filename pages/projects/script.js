c695cc24f3779a0998c7479cfd5e60b0e8a84a13
Karl Shrouder <karl.shrouder@gmail.com>
2023-01-26 21:27:25 -0500

    Current projects made into cards

---
+++ pages/projects/script.js
@@ -0,0 +1,7 @@
+document.getElementById("calculator").addEventListener("click", function(){
+    window.open("./calculator/calculator.html")
+});
+
+document.getElementById("math").addEventListener("click", function(){
+    window.open("./math-for-kids/index.html")
+});
