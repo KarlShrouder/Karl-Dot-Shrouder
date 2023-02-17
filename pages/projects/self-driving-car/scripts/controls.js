56d7cbade90e07b1eb319ab3007d91ca9642d361
Karl Shrouder <karl.shrouder@gmail.com>
2023-02-17 16:11:51 -0500

    Self Driving Project: Car environment

--- pages/projects/self-driving-car/scripts/controls.js
+++ pages/projects/self-driving-car/scripts/controls.js
@@ -1,45 +1,53 @@
 class Controls {
-    constructor() {
+    constructor(type) {
         this.forward = false;
         this.left = false;
         this.backward = false;
         this.right = false;
 
-        this.#addKeyboardListeners();
+        switch (type) {
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward = true;
+                break;
+        }
+
     }
 
     #addKeyboardListeners() {
-       document.onkeydown = (e) => {
-        switch(e.key) {
-            case "ArrowUp":
-                this.forward = true;
-                break;
-            case "ArrowLeft":
-                this.left = true;
-                break;
-            case "ArrowDown":
-                this.backward = true;
-                break;
-            case "ArrowRight":
-                this.right = true;
-                break;
+        document.onkeydown = (e) => {
+            switch (e.key) {
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowDown":
+                    this.backward = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+            }
         }
-       }
-       document.onkeyup = (e) => {
-        switch(e.key) {
-            case "ArrowUp":
-                this.forward = false;
-                break;
-            case "ArrowLeft":
-                this.left = false;
-                break;
-            case "ArrowDown":
-                this.backward = false;
-                break;
-            case "ArrowRight":
-                this.right = false;
-                break;
+        document.onkeyup = (e) => {
+            switch (e.key) {
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowDown":
+                    this.backward = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+            }
         }
-       }
     }
 }
